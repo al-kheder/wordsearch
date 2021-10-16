1938ecf0f9bac251897e4d3835fb662634869850
al-kheder <alkhdyyr@gmail.com>
2021-10-16 17:52:01 +0200

    Wordsearch API Implementation

---
+++ src/test/java/io/learnproject/wordsearchapi/WordsearchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.learnproject.wordsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsearchapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
